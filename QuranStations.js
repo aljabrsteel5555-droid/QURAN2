34e2e0e1cfb55954b8eaf0d4b1f9ca6686e60abe
aljabrsteel5555-droid <aljabrsteel5555@gmail.com>
2025-11-24 16:15:58 +0200

    Update QuranStations.js

--- QuranStations.js
+++ QuranStations.js
@@ -1,53 +1,46 @@
 const Stations = [
   {
     id: "1",
-    name: "إذاعة صور",
-    radio_url: "https://qurango.net/radio/sahabah",
+    name: "الحصري",
+    radio_url: "https://server03.quran-uni.com:7007/;*.mp3",
     name_en: "Sahaba Stories",
     category: "منوعات",
     category_en: "Others"
   },
   {
     id: "2",
-    name: "تراتيل قصيرة متميزة",
-    radio_url: "https://qurango.net/radio/tarateel",
+    name: "المنشاوي",
+    radio_url: "https://server03.quran-uni.com:7036/;*.mp3",
     name_en: "Amazing short Recitations",
     category: "منوعات",
     category_en: "Others"
   },
   {
     id: "3",
-    name: "إذاعة آيات السكينة",
-    radio_url: "https://qurango.net/radio/sakeenah",
+    name: "البنا",
+    radio_url: "https://server03.quran-uni.com:7010/;*.mp3",
     name_en: "Calm Recitations",
     category: "منوعات",
     category_en: "Others"
   },
   {
     id: "154",
-    name: "إذاعة القرآن الكريم من القاهرة",
-    radio_url: "https://stream.radiojar.com/8s5u5tpdtwzuv",
+    name: "مصطفي اسماعيل",
+    radio_url: "https://server03.quran-uni.com:7132/;*.mp3",
     name_en: "Quran Radio  Cairo",
     category: "منوعات",
     category_en: "Others"
   },
   {
     id: "155",
-    name: "إذاعة القرآن الكريم من المملكة العربية السعودية",
-    radio_url: "https://stream.radiojar.com/0tpy1h0kxtzuv",
+    name: "عبدالباسط",
+    radio_url: "https://server03.quran-uni.com:7000/;*.mp3",
     name_en: "Quran Radio  Saudi Arabia",
-    category: "منوعات",
-    category_en: "Others"
-  },
-  {
-    id: "156",
-    name: "إذاعة الحرم المكي",
-    radio_url: "http://r7.tarat.com:8004/stream?type=http&nocache=114",
-    name_en: "Masjid Al Haram Radio",
     category: "منوعات",
     category_en: "Others"
   }
   ]
 
 module.exports = { Stations }
+
 
