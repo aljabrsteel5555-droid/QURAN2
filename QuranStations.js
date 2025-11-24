ceb3194e55b5b38aee9c21289834aa6d1330009f
aljabrsteel5555-droid <aljabrsteel5555@gmail.com>
2025-11-24 14:45:50 +0200

    Update QuranStations.js

--- QuranStations.js
+++ QuranStations.js
@@ -1,44 +1,52 @@
 const Stations = [
   {
     id: "1",
-    name: "محمود خليل الحصري",
-    radio_url: "https://server03.quran-uni.com:7007/;*.mp3",
-    name_en: "ELHOSARY",
-    category: "القراء الخمسة",
+    name: "إذاعة صور من حياة الصحابة رضوان الله عليهم",
+    radio_url: "https://qurango.net/radio/sahabah",
+    name_en: "Sahaba Stories",
+    category: "منوعات",
     category_en: "Others"
   },
   {
     id: "2",
-    name: "محمد صديق المنشاوي",
-    radio_url: "https://server03.quran-uni.com:7036/;*.mp3",
-    name_en: "ALMANSHAWY",
-    category: "القراء الخمسة",
+    name: "تراتيل قصيرة متميزة",
+    radio_url: "https://qurango.net/radio/tarateel",
+    name_en: "Amazing short Recitations",
+    category: "منوعات",
     category_en: "Others"
   },
   {
     id: "3",
-    name: "محمود علي البنا",
-    radio_url: "https://server03.quran-uni.com:7010/;*.mp3",
-    name_en: "EL BANNA",
-    category: "القراء الخمسة",
+    name: "إذاعة آيات السكينة",
+    radio_url: "https://qurango.net/radio/sakeenah",
+    name_en: "Calm Recitations",
+    category: "منوعات",
     category_en: "Others"
   },
   {
-    id: "4",
-    name: "مصطفي إسماعيل",
-    radio_url: "https://server03.quran-uni.com:7132/;*.mp3",
-    name_en: "MOSTAFA ESMAIL",
-    category: "القراء الخمسة",
+    id: "154",
+    name: "إذاعة القرآن الكريم من القاهرة",
+    radio_url: "https://stream.radiojar.com/8s5u5tpdtwzuv",
+    name_en: "Quran Radio  Cairo",
+    category: "منوعات",
     category_en: "Others"
   },
   {
-    id: "5",
-    name: "عبد الباسط عبد الصمد",
-    radio_url: "https://server03.quran-uni.com:7000/;*.mp3",
-    name_en: "ABDELBASE ABDELSAMAD",
-    category: "القراء الخمسة",
+    id: "155",
+    name: "إذاعة القرآن الكريم من المملكة العربية السعودية",
+    radio_url: "https://stream.radiojar.com/0tpy1h0kxtzuv",
+    name_en: "Quran Radio  Saudi Arabia",
+    category: "منوعات",
+    category_en: "Others"
+  },
+  {
+    id: "156",
+    name: "إذاعة الحرم المكي",
+    radio_url: "http://r7.tarat.com:8004/stream?type=http&nocache=114",
+    name_en: "Masjid Al Haram Radio",
+    category: "منوعات",
     category_en: "Others"
   }
-]
+  ]
 
 module.exports = { Stations }
