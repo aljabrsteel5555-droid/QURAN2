6051e381fffaad76560d1b5b13a727dcfd83ce73
aljabrsteel5555-droid <aljabrsteel5555@gmail.com>
2025-11-24 12:44:12 +0200

    Update QuranStations.js

--- QuranStations.js
+++ QuranStations.js
@@ -1,36 +1,45 @@
 const Stations = [
   {
     id: "1",
-    name: "الحصري",
+    name: "محمود خليل الحصري",
     radio_url: "https://server03.quran-uni.com:7007/;*.mp3",
     name_en: "ELHOSARY",
-    category: "منوعات",
+    category: "القراء الخمسة",
     category_en: "Others"
   },
   {
     id: "2",
-    name: "تراتيل قصيرة متميزة",
-    radio_url: "https://qurango.net/radio/tarateel",
-    name_en: "Amazing short Recitations",
-    category: "منوعات",
+    name: "محمد صديق المنشاوي",
+    radio_url: "https://server03.quran-uni.com:7036/;*.mp3",
+    name_en: "ALMANSHAWY",
+    category: "القراء الخمسة",
     category_en: "Others"
   },
   {
     id: "3",
-    name: "إذاعة آيات السكينة",
-    radio_url: "https://qurango.net/radio/sakeenah",
-    name_en: "Calm Recitations",
-    category: "منوعات",
+    name: "محمود علي البنا",
+    radio_url: "https://server03.quran-uni.com:7010/;*.mp3",
+    name_en: "EL BANNA",
+    category: "القراء الخمسة",
     category_en: "Others"
   },
   {
     id: "154",
-    name: "إذاعة القرآن الكريم من القاهرة",
-    radio_url: "https://stream.radiojar.com/8s5u5tpdtwzuv",
-    name_en: "Quran Radio  Cairo",
-    category: "منوعات",
+    name: "مصطفي إسماعيل",
+    radio_url: "https://server03.quran-uni.com:7132/;*.mp3",
+    name_en: "MOSTAFA ESMAIL",
+    category: "القراء الخمسة",
+    category_en: "Others"
+ },
+  {
+    id: "154",
+    name: "عبد الباسط عبد الصمد",
+    radio_url: "https://server03.quran-uni.com:7000/;*.mp3",
+    name_en: "ABDELBASE ABDELSAMAD",
+    category: "القراء الخمسة",
     category_en: "Others"
 }
 ]
 
 module.exports = { Stations }
+
